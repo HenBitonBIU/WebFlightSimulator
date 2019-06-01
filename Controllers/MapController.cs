77f706ca276af822fe2e7eaac62186460fcf145f
HenBitonBIU <45950041+HenBitonBIU@users.noreply.github.com>
2019-06-01 15:34:27 +0300

    Add files via upload

---
+++ Controllers/MapController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ajax_Minimal.Models;
+using System.Xml;
+using System.Text;
+using System.Net;
+
+namespace Ajax_Minimal.Controllers
+{
+    public class MapController : Controller
+    {
+        const int MaxTime = 1000000;
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult display(string ip, int port, int time = MaxTime)
+        {
+            IPAddress ipAdd;
+            if (IPAddress.TryParse(ip, out ipAdd))
+            {
+                if (ipAdd.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6
+                    || ipAdd.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    InfoModel.Instance.NetworkConnection.Ip = ip;
+                    InfoModel.Instance.NetworkConnection.Port = port;
+                    InfoModel.Instance.Time = time;
+                    InfoModel.Instance.ConnectNetwork(); // connect to server for reading.
+
+                    Session["time"] = time;
+                    Session["isNetworkDisplay"] = "1";
+                    return View();
+                }
+            }
+            InfoModel.Instance.FilePath = ip;
+            InfoModel.Instance.Time = time;
+            Session["time"] = time;
+            Session["isNetworkDisplay"] = "0";
+
+            return View();
+        }
+
+        public ActionResult save(string ip, int port, int time, int timeout, string filePath)
+        {
+            InfoModel.Instance.NetworkConnection.Ip = ip;
+            InfoModel.Instance.NetworkConnection.Port = port;
+            InfoModel.Instance.Time = time;
+            InfoModel.Instance.Timeout = timeout;
+            InfoModel.Instance.FilePath = AppDomain.CurrentDomain.BaseDirectory + filePath + ".csv";
+            InfoModel.Instance.ConnectNetwork(); // connect to server for reading.
+            Session["time"] = time;
+            Session["timeout"] = timeout;
+            return View();
+        }
+
+        [HttpPost]
+        public string OpenNewFile()
+        {
+            string fileName = InfoModel.Instance.FilePath;
+            InfoModel.Instance.CreateFile(fileName);
+
+            return fileName;
+        }
+
+        [HttpPost]
+        public string WriteToFile()
+        {
+            string fileName = InfoModel.Instance.FilePath;
+            InfoModel.Instance.WriteToFile(fileName);
+
+            return fileName;
+        }
+
+        [HttpPost]
+        public string CloseFile()
+        {
+            string fileName = InfoModel.Instance.FilePath;
+            InfoModel.Instance.CloseFile(fileName);
+
+            return fileName;
+        }
+
+        // These function initializes an XML format of the Network object.
+        [HttpPost]
+        public string GetNetwork()
+        {
+            var network = InfoModel.Instance.NetworkConnection;
+
+            network.Write();
+
+            return ToXml(network);
+        }
+        public void Disconnect()
+        {
+            var network = InfoModel.Instance.NetworkConnection;
+            network.Disconnect();
+        }
+        private string ToXml(NetworkConnection network)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            XmlWriter writer = XmlWriter.Create(sb, settings);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("NetworkConnections");
+            network.ToXml(writer);
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Flush();
+            return sb.ToString();
+        }
+    }
+}
---
+++ Models/InfoModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ajax_Minimal.Models;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+
+namespace Ajax_Minimal.Models
+{
+    public class InfoModel
+    {
+        private static InfoModel s_instace = null;
+
+        public static InfoModel Instance
+        {
+            get
+            {
+                if (s_instace == null)
+                {
+                  //  Debug.WriteLine("network in info was null");
+                    s_instace = new InfoModel();
+                }
+               // Debug.WriteLine("Returning network - from the GetNetwrok retquest from js");
+                return s_instace;
+            }
+        }
+
+        public int Timeout { get; set; }
+        public int Time { get; set; }
+        public string FilePath { get; set; }
+        private StreamWriter streamWriter;
+
+        public NetworkConnection NetworkConnection { get; private set; }
+
+        private InfoModel()
+        {
+            NetworkConnection = new NetworkConnection();
+        }
+
+        public void ConnectNetwork()
+        {
+            NetworkConnection.Connect();
+        }
+
+        public void CreateFile(string filePath)
+        {
+            this.streamWriter = new StreamWriter(filePath);
+        }
+
+        public void WriteToFile(string filePath)
+        {
+            string toWrite = this.NetworkConnection.Lon.ToString() + "," + this.NetworkConnection.Lat.ToString();
+            this.streamWriter.WriteLineAsync(toWrite); // the writing needs to be done in another func.
+        }
+
+        public void CloseFile(string filePath)
+        {
+            this.streamWriter.Close();
+        }
+    }
+}
